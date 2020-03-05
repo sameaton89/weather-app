474b14fa4f7f18bd8d704420563f47986be2e87b
Sam Eaton <sameaton1989@gmail.com>
2020-03-04 20:17:16 -0500

    Added high and low temperatures.

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -14,7 +14,8 @@ const forecast = (latitude, longitude, callback) => {
         } else if (body.error) {
             callback('Unable to find location.', undefined);
         } else {
-            callback(undefined, `${body.daily.data[0].summary} It is currently ${body.currently.temperature} degrees out. There is ${body.currently.precipProbability}% chance of rain.`)
+            console.log(body.daily.data[0]);
+            callback(undefined, `${body.daily.data[0].summary} It is currently ${body.currently.temperature} degrees out. There is ${body.currently.precipProbability}% chance of rain. <p> Highs of ${body.daily.data[0].temperatureHigh}°F. Lows of ${body.daily.data[0].temperatureLow}°F.`)
         }
     });
 }
